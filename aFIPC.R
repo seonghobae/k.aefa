e3ea6c442819503e0d76764379d38a16286b6fbe
Seongho <seongho@kw.ac.kr>
2016-06-16 16:54:47 +0900

    debug

--- aFIPC.R
+++ aFIPC.R
@@ -188,7 +188,7 @@ autoFIPC <- function(newformXData = ..., oldformYData = ..., newformCommonItemNa
   message('with Cai\'s (2010) Metropolis-Hastings Robbins-Monro (MHRM) approach. please be patient.')
   LinkedModelSyntax <- mirt::mirt.model(paste0('F1 = 1-',ncol(newformXData[colnames(newFormModel@Data$data)]),'\n',
                                                'MEAN = F1'))
-  LinkedModel <- mirt::mirt(data = newformXData[colnames(newFormModel@Data$data)], LinkedModelSyntax, itemtype = newFormModel@Model$itemtype, SE = T, SE.type = 'MHRM', method = 'MHRM', accelerate = 'squarem', technical = list(NCYCLES = 1e+5, MHRM_SE_draws = 1e+5), pars = NewScaleParms, GenRandomPars = F, )
+  LinkedModel <- mirt::mirt(data = newformXData[colnames(newFormModel@Data$data)], LinkedModelSyntax, itemtype = newFormModel@Model$itemtype, SE = T, accelerate = 'squarem', technical = list(NCYCLES = 1e+5, MHRM_SE_draws = 1e+5), pars = NewScaleParms, GenRandomPars = F)
   
   # if(!LinkedModel@OptimInfo$secondordertest){
   #   message('Estimation failed. estimating new parameters with no prior distribution using quasi-Monte Carlo EM estimation. please be patient.')
