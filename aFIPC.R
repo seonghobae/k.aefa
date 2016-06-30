c3af775f74588a16d0da19ba6b2e839ce4069f46
Seongho <seongho@kw.ac.kr>
2016-06-30 17:04:28 +0900

    initial support for pre-computed mirt::mirt model

--- aFIPC.R
+++ aFIPC.R
@@ -3,7 +3,7 @@ source('https://raw.githubusercontent.com/seonghobae/k.aefa/master/k.aefa3.R')
 autoFIPC <- function(newformXData = ..., oldformYData = ..., newformCommonItemNames = ..., oldformCommonItemNames = ..., itemtype = '3PL', newformBILOGprior = NULL, oldformBILOGprior = NULL, tryFitwholeNewItems = T, tryFitwholeOldItems = T, ...){
   
   # print credits
-  message('automated Fixed Item Parameter Calibration: aFIPC 0.1')
+  message('automated Fixed Item Parameter Calibration: aFIPC 0.2')
   message('Seongho Bae (seongho@kw.ac.kr)\n')
   
   # checking configure
@@ -38,135 +38,148 @@ autoFIPC <- function(newformXData = ..., oldformYData = ..., newformCommonItemNa
   }
   
   # estimate models for calibration
-  
-  if(itemtype == '3PL' && length(oldformBILOGprior) == 0){
-    checkoldformBILOGprior <- function()
-    { 
-      n <- readline(prompt="Do you want to use default BILOG-MG priors for oldform Data? (1: Yes 2: No) : ")
-      if(!grepl("^[0-9]+$",n))
-      {
-        return(readinteger())
+  if(is.list(oldformYData@Options) && !is.data.frame(oldformYData)){ # if Data is mirt model
+    oldFormModel <- oldformYData
+    oldformYDataK <- data.frame(oldFormModel@Data$data)
+  } else { # if Data is data.frame
+    oldformYDataK <- oldformYData
+    if(itemtype == '3PL' && length(oldformBILOGprior) == 0){
+      checkoldformBILOGprior <- function()
+      { 
+        n <- readline(prompt="Do you want to use default BILOG-MG priors for oldform Data? (1: Yes 2: No) : ")
+        if(!grepl("^[0-9]+$",n))
+        {
+          return(readinteger())
+        }
+        
+        return(as.integer(n))
+      }
+      oldformBILOGprior <- checkoldformBILOGprior()
+      if(oldformBILOGprior == 1){
+        oldformBILOGprior <- TRUE
+      } else {
+        oldformBILOGprior <- FALSE
+      }
+    }
+    
+    message('\nestimating oldForm (Y) parameters')
+    if(itemtype == '3PL' && oldformBILOGprior == T){
+      message('with traditional MMLE/EM approach')
+      oldFormModelSyntax <- mirt::mirt.model(paste0('F1 = 1-',ncol(oldformYData),'\n',
+                                                    'PRIOR = (1-',ncol(oldformYData),', a1, lnorm, 1, 1.6487), ', '(1-',ncol(oldformYData),', g, norm, .22, .08)'))
+      try(oldFormModel <- mirt::mirt(data = oldformYData, model = oldFormModelSyntax, itemtype = itemtype, SE = T, accelerate = 'squarem'))
+    } else { # try to search priors automatically. if it fail, try to bayesian approaches
+      message('with estimate prior distribution using an empirical histogram approach. please be patient.')
+      try(oldFormModel <- mirt::mirt(data = oldformYData, model = 1, itemtype = itemtype, SE = T, accelerate = 'squarem', empiricalhist = T, technical = list(NCYCLES = 1e+5), GenRandomPars = F))
+      
+    }
+    
+    if(tryFitwholeOldItems == T){
+      if(!oldFormModel@OptimInfo$secondordertest){
+        message('Estimation failed. estimating new parameters with no prior distribution using quasi-Monte Carlo EM estimation. please be patient.')
+        
+        try(rm(oldFormModel))
+        try(oldFormModel <- mirt::mirt(data = oldformYData, 1, itemtype = itemtype, SE = T, method = 'QMCEM', accelerate = 'squarem', technical = list(NCYCLES = 1e+5), GenRandomPars = F))
       }
       
-      return(as.integer(n))
+      if(!oldFormModel@OptimInfo$secondordertest){
+        message('Estimation failed. estimating new parameters with no prior distribution using  Cai\'s (2010) Metropolis-Hastings Robbins-Monro (MHRM) algorithm. please be patient.')
+        
+        try(rm(oldFormModel))
+        while (!exists('oldFormModel')) {
+          try(oldFormModel <- mirt::mirt(data = oldformYData, 1, itemtype = itemtype, SE = T, method = 'MHRM', accelerate = 'squarem', technical = list(NCYCLES = 1e+5, MHRM_SE_draws = 200000), GenRandomPars = F))
+        }
+      }
     }
-    oldformBILOGprior <- checkoldformBILOGprior()
-    if(oldformBILOGprior == 1){
-      oldformBILOGprior <- TRUE
-    } else {
-      oldformBILOGprior <- FALSE
-    }
-  }
-  
-  message('\nestimating oldForm (Y) parameters')
-  if(itemtype == '3PL' && oldformBILOGprior == T){
-    message('with traditional MMLE/EM approach')
-    oldFormModelSyntax <- mirt::mirt.model(paste0('F1 = 1-',ncol(oldformYData),'\n',
-                                                  'PRIOR = (1-',ncol(oldformYData),', a1, lnorm, 1, 1.6487), ', '(1-',ncol(oldformYData),', g, norm, .22, .08)'))
-    try(oldFormModel <- mirt::mirt(data = oldformYData, model = oldFormModelSyntax, itemtype = itemtype, SE = T, accelerate = 'squarem'))
-  } else { # try to search priors automatically. if it fail, try to bayesian approaches
-    message('with estimate prior distribution using an empirical histogram approach. please be patient.')
-    try(oldFormModel <- mirt::mirt(data = oldformYData, model = 1, itemtype = itemtype, SE = T, accelerate = 'squarem', empiricalhist = T, technical = list(NCYCLES = 1e+5), GenRandomPars = F))
     
-  }
-  
-  if(tryFitwholeOldItems == T){
     if(!oldFormModel@OptimInfo$secondordertest){
-      message('Estimation failed. estimating new parameters with no prior distribution using quasi-Monte Carlo EM estimation. please be patient.')
-      
+      message('Estimation failed. trying to remove weird items by itemfit statistics')
       try(rm(oldFormModel))
-      try(oldFormModel <- mirt::mirt(data = oldformYData, 1, itemtype = itemtype, SE = T, method = 'QMCEM', accelerate = 'squarem', technical = list(NCYCLES = 1e+5), GenRandomPars = F))
+      
+      oldFormModel <- surveyFA(oldformYData, autofix = F, SE = T, forceMHRM = T)
     }
     
     if(!oldFormModel@OptimInfo$secondordertest){
-      message('Estimation failed. estimating new parameters with no prior distribution using  Cai\'s (2010) Metropolis-Hastings Robbins-Monro (MHRM) algorithm. please be patient.')
-      
-      try(rm(oldFormModel))
-      while (!exists('oldFormModel')) {
-        try(oldFormModel <- mirt::mirt(data = oldformYData, 1, itemtype = itemtype, SE = T, method = 'MHRM', accelerate = 'squarem', technical = list(NCYCLES = 1e+5, MHRM_SE_draws = 200000), GenRandomPars = F))
-      }
+      stop('Estimation failed. Please check test quality.')
     }
-  }
-  
-  if(!oldFormModel@OptimInfo$secondordertest){
-    message('Estimation failed. trying to remove weird items by itemfit statistics')
-    try(rm(oldFormModel))
     
-    oldFormModel <- surveyFA(oldformYData, autofix = F, SE = T, forceMHRM = T)
   }
   
-  if(!oldFormModel@OptimInfo$secondordertest){
-    stop('Estimation failed. Please check test quality.')
-  }
-  
-  if(itemtype == '3PL' && length(newformBILOGprior) == 0){
-    checknewformBILOGprior <- function()
-    { 
-      n <- readline(prompt="Do you want to use default BILOG-MG priors for newform Data? (1: Yes 2: No) : ")
-      if(!grepl("^[0-9]+$",n))
-      {
-        return(checknewformBILOGprior())
-      }
-      
-      return(as.integer(n))
-    }
-    newformBILOGprior <- checknewformBILOGprior()
-    if(newformBILOGprior == 1){
-      newformBILOGprior <- TRUE
-    } else {
-      newformBILOGprior <- FALSE
-    }
-  }
-  
-  
-  message('\nestimating newForm (X) parameters')
-  if(itemtype == '3PL' && newformBILOGprior == T){
-    message('with traditional MMLE/EM approach')
-    newFormModelSyntax <- mirt::mirt.model(paste0('F1 = 1-',ncol(newformXData),'\n',
-                                                  'PRIOR = (1-',ncol(newformXData),', a1, lnorm, 1, 1.6487), ', '(1-',ncol(newformXData),', g, norm, .22, .08)'))
-    try(newFormModel <- mirt::mirt(data = newformXData, model = newFormModelSyntax, itemtype = itemtype, SE = T, accelerate = 'squarem'))
+  if(is.list(newformXData@Options) && !is.data.frame(newformXData)){ # if Data is mirt model
+    newFormModel <- newformXData
+    newformXDataK <- data.frame(newFormModel@Data$data)
   } else {
-    message('with estimate prior distribution using an empirical histogram approach. please be patient.')
-    try(newFormModel <- mirt::mirt(data = newformXData, 1, itemtype = itemtype, SE = T, empiricalhist = T, accelerate = 'squarem', technical = list(NCYCLES = 1e+5), GenRandomPars = F))
-  }
-  
-  if (tryFitwholeNewItems) {
-    
-    if(!newFormModel@OptimInfo$secondordertest){
-      message('Estimation failed. estimating new parameters with no prior distribution using quasi-Monte Carlo EM estimation. please be patient.')
-      
-      try(rm(newFormModel))
-      try(newFormModel <- mirt::mirt(data = newformXData, 1, itemtype = itemtype, SE = T, method = 'QMCEM', accelerate = 'squarem', technical = list(NCYCLES = 1e+5), GenRandomPars = F))
-    }
-    
-    if(!newFormModel@OptimInfo$secondordertest){
-      message('Estimation failed. estimating new parameters with no prior distribution using  Cai\'s (2010) Metropolis-Hastings Robbins-Monro (MHRM) algorithm. please be patient.')
-      
-      try(rm(newFormModel))
-      while (!exists('newFormModel')) {
-        try(newFormModel <- mirt::mirt(data = newformXData, 1, itemtype = itemtype, SE = T, method = 'MHRM', accelerate = 'squarem', technical = list(NCYCLES = 1e+5, MHRM_SE_draws = 200000), GenRandomPars = F))
+    newformYDataK <- newformYData
+    if(itemtype == '3PL' && length(newformBILOGprior) == 0){
+      checknewformBILOGprior <- function()
+      { 
+        n <- readline(prompt="Do you want to use default BILOG-MG priors for newform Data? (1: Yes 2: No) : ")
+        if(!grepl("^[0-9]+$",n))
+        {
+          return(checknewformBILOGprior())
+        }
+        
+        return(as.integer(n))
+      }
+      newformBILOGprior <- checknewformBILOGprior()
+      if(newformBILOGprior == 1){
+        newformBILOGprior <- TRUE
+      } else {
+        newformBILOGprior <- FALSE
       }
     }
     
-  }
-  
-  if(!newFormModel@OptimInfo$secondordertest){
-    message('Estimation failed. trying to remove weird items by itemfit statistics')
-    try(rm(newFormModel))
     
-    newFormModel <- surveyFA(newformXData, autofix = F, SE = T, forceMHRM = T)
+    message('\nestimating newForm (X) parameters')
+    if(itemtype == '3PL' && newformBILOGprior == T){
+      message('with traditional MMLE/EM approach')
+      newFormModelSyntax <- mirt::mirt.model(paste0('F1 = 1-',ncol(newformXData),'\n',
+                                                    'PRIOR = (1-',ncol(newformXData),', a1, lnorm, 1, 1.6487), ', '(1-',ncol(newformXData),', g, norm, .22, .08)'))
+      try(newFormModel <- mirt::mirt(data = newformXData, model = newFormModelSyntax, itemtype = itemtype, SE = T, accelerate = 'squarem'))
+    } else {
+      message('with estimate prior distribution using an empirical histogram approach. please be patient.')
+      try(newFormModel <- mirt::mirt(data = newformXData, 1, itemtype = itemtype, SE = T, empiricalhist = T, accelerate = 'squarem', technical = list(NCYCLES = 1e+5), GenRandomPars = F))
+    }
+    
+    if (tryFitwholeNewItems) {
+      
+      if(!newFormModel@OptimInfo$secondordertest){
+        message('Estimation failed. estimating new parameters with no prior distribution using quasi-Monte Carlo EM estimation. please be patient.')
+        
+        try(rm(newFormModel))
+        try(newFormModel <- mirt::mirt(data = newformXData, 1, itemtype = itemtype, SE = T, method = 'QMCEM', accelerate = 'squarem', technical = list(NCYCLES = 1e+5), GenRandomPars = F))
+      }
+      
+      if(!newFormModel@OptimInfo$secondordertest){
+        message('Estimation failed. estimating new parameters with no prior distribution using  Cai\'s (2010) Metropolis-Hastings Robbins-Monro (MHRM) algorithm. please be patient.')
+        
+        try(rm(newFormModel))
+        while (!exists('newFormModel')) {
+          try(newFormModel <- mirt::mirt(data = newformXData, 1, itemtype = itemtype, SE = T, method = 'MHRM', accelerate = 'squarem', technical = list(NCYCLES = 1e+5, MHRM_SE_draws = 200000), GenRandomPars = F))
+        }
+      }
+      
+    }
+    
+    if(!newFormModel@OptimInfo$secondordertest){
+      message('Estimation failed. trying to remove weird items by itemfit statistics')
+      try(rm(newFormModel))
+      
+      newFormModel <- surveyFA(newformXData, autofix = F, SE = T, forceMHRM = T)
+    }
+    
+    if(!newFormModel@OptimInfo$secondordertest){
+      stop('Estimation failed. Please check test quality.')
+    }
   }
-  
-  if(!newFormModel@OptimInfo$secondordertest){
-    stop('Estimation failed. Please check test quality.')
-  }
+
   
   # do FIPC
   NewScaleParms <- mod2values(newFormModel)
   OldScaleParms <- mod2values(oldFormModel)
   
   for(i in 1:length(oldformCommonItemNames)){
-    if((length(grep(paste0('^',newformCommonItemNames[i],'$'), colnames(newformXData[colnames(newFormModel@Data$data)]))) == 1) == TRUE && (length(grep(paste0('^',oldformCommonItemNames[i],'$'), colnames(oldformYData[colnames(oldFormModel@Data$data)]))) == 1) == TRUE){
+    if((length(grep(paste0('^',newformCommonItemNames[i],'$'), colnames(newformXDataK[colnames(newFormModel@Data$data)]))) == 1) == TRUE && (length(grep(paste0('^',oldformCommonItemNames[i],'$'), colnames(oldformYDataK[colnames(oldFormModel@Data$data)]))) == 1) == TRUE){
       message('applying ', paste0(newformCommonItemNames[i]), ' <<< ', paste0(oldformCommonItemNames[i]), ' as common item use')
       
       message('   Newform Parms: ', paste0(NewScaleParms[which(NewScaleParms$item == paste0(newformCommonItemNames[i])), "value"], ' '))
@@ -186,15 +199,15 @@ autoFIPC <- function(newformXData = ..., oldformYData = ..., newformCommonItemNa
   
   message('\nestimating Linked Form Eq(X) parameters')
   message('with Cai\'s (2010) Metropolis-Hastings Robbins-Monro (MHRM) approach. please be patient.')
-  LinkedModelSyntax <- mirt::mirt.model(paste0('F1 = 1-',ncol(newformXData[colnames(newFormModel@Data$data)]),'\n',
+  LinkedModelSyntax <- mirt::mirt.model(paste0('F1 = 1-',ncol(newformXDataK[colnames(newFormModel@Data$data)]),'\n',
                                                'MEAN = F1'))
-  LinkedModel <- mirt::mirt(data = newformXData[colnames(newFormModel@Data$data)], LinkedModelSyntax, itemtype = newFormModel@Model$itemtype, method = 'MHRM', SE = T, accelerate = 'squarem', technical = list(NCYCLES = 1e+5, MHRM_SE_draws = 1e+5), pars = NewScaleParms, GenRandomPars = F)
+  LinkedModel <- mirt::mirt(data = newformXDataK[colnames(newFormModel@Data$data)], LinkedModelSyntax, itemtype = newFormModel@Model$itemtype, method = 'MHRM', SE = T, accelerate = 'squarem', TOL = 1e-4, technical = list(NCYCLES = 1e+5, MHRM_SE_draws = 1e+5), pars = NewScaleParms, GenRandomPars = F)
   
   # if(!LinkedModel@OptimInfo$secondordertest){
   #   message('Estimation failed. estimating new parameters with no prior distribution using quasi-Monte Carlo EM estimation. please be patient.')
   #   
   #   rm(LinkedModel)
-  #   try(LinkedModel <- mirt::mirt(data = newformXData[colnames(newFormModel@Data$data)], LinkedModelSyntax, itemtype = newFormModel@Model$itemtype, SE = T, method = 'QMCEM', accelerate = 'squarem', technical = list(NCYCLES = 1e+5), pars = NewScaleParms, GenRandomPars = F))
+  #   try(LinkedModel <- mirt::mirt(data = newformXDataK[colnames(newFormModel@Data$data)], LinkedModelSyntax, itemtype = newFormModel@Model$itemtype, SE = T, method = 'QMCEM', accelerate = 'squarem', technical = list(NCYCLES = 1e+5), pars = NewScaleParms, GenRandomPars = F))
   # }
   # 
   # if(!LinkedModel@OptimInfo$secondordertest){
@@ -202,7 +215,7 @@ autoFIPC <- function(newformXData = ..., oldformYData = ..., newformCommonItemNa
   #   
   #   rm(LinkedModel)
   #   while (!exists('LinkedModel')) {
-  #     try(LinkedModel <- mirt::mirt(data = newformXData[colnames(newFormModel@Data$data)], LinkedModelSyntax, itemtype = newFormModel@Model$itemtype, SE = T, method = 'MHRM', accelerate = 'squarem', technical = list(NCYCLES = 1e+5, MHRM_SE_draws = 200000), pars = NewScaleParms, GenRandomPars = T))
+  #     try(LinkedModel <- mirt::mirt(data = newformXDataK[colnames(newFormModel@Data$data)], LinkedModelSyntax, itemtype = newFormModel@Model$itemtype, SE = T, method = 'MHRM', accelerate = 'squarem', technical = list(NCYCLES = 1e+5, MHRM_SE_draws = 200000), pars = NewScaleParms, GenRandomPars = T))
   #   }
   # }
   
