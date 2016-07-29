eb7a95c232520fa5f4be9f376adb20e82367c5e4
Seongho <seongho@kw.ac.kr>
2016-07-29 10:58:28 +0900

    adjust n items for testing item fit

--- k.aefa3.R
+++ k.aefa3.R
@@ -1925,7 +1925,7 @@ surveyFA <- function(data = ..., covdata = NULL, formula = NULL, SE = F,
     surveyFixModRAW <- data.frame(mirt::extract.mirt(surveyFixMod, 'data'))
     surveyFixModCOV <- data.frame(attr(surveyFixMod@ParObjects$lrPars, "df"))
     
-    if(ncol(surveyFixModRAW) > 5){
+    if(ncol(surveyFixModRAW) > 3){
       
       
       message('\nChecking item local independence assumption')
