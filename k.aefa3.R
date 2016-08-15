e294757d7643ecdfdeaa002465abc511d585291e
Seongho <seongho@kw.ac.kr>
2016-08-15 12:04:00 +0900

    add fail safe

--- k.aefa3.R
+++ k.aefa3.R
@@ -2815,10 +2815,12 @@ deepFA <- function(mirtModel){ # for search more factors with prevent local opti
     
     try(invisible(tempModel <- mirt::mirt(data = mirtModel@Data$data, model = i, itemtype = mirtModel@Model$itemtype, SE = mirtModel@Options$SE, SE.type = mirtModel@Options$SE.type, covdata = attr(mirtModel@ParObjects$lrPars, 'df'), formula = attr(mirtModel@ParObjects$lrPars, 'formula')[[1]], method = method, optimizer = mirtModel@Options$Moptim, accelerate = mirtModel@Options$accelerate, verbose = F, technical = list(NCYCLES = mirtModel@Options$technical$NCYCLES, MAXQUAD = mirtModel@Options$technical$MAXQUAD, SEtol = mirtModel@Options$technical$SEtol, symmetric_SEM = mirtModel@Options$technical$symmetric_SEM, removeEmptyRows = mirtModel@Options$technical$removeEmptyRows, MHRM_SE_draws = mirtModel@Options$technical$MHRM_SE_draws))), silent = T)
     if(exists('tempModel')){
-      j <- j + 1
-      DICindices[j] <- tempModel@Fit$DIC
-      nfact[j] <- i
-      rm(tempModel)
+      if(tempModel@OptimInfo$converged){
+        j <- j + 1
+        DICindices[j] <- tempModel@Fit$DIC
+        nfact[j] <- i
+        rm(tempModel)
+      }
     }
   }
   bestModel <- which(min(DICindices) == DICindices)
