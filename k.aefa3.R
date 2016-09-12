c473dda9bf5e268705c9c36cc61b58a1bdc88848
Seongho <seongho@kw.ac.kr>
2016-09-12 22:47:57 +0900

    PC3PL isn't need the UIRT setting too.

--- k.aefa3.R
+++ k.aefa3.R
@@ -1335,7 +1335,7 @@ fastFIFA <- function(x, covdata = NULL, formula = NULL, SE = F, SE.type = "cross
           }
         }
         
-        if(exists('modTEMP') == F){
+        if(exists('modTEMP') == F && i > 1){
           
           message('\nMIRT model: Partially compensatory 3PL')
           try(modTEMP <- mirt::mirt(data = x, model = i, itemtype = 'PC3PL', method = estimationMETHOD,
