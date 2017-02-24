be2f1f1c5dfefea6f1287243eec2192ec8f82f91
Seongho <seongho@kw.ac.kr>
2017-02-24 22:57:30 +0900

    make stabilizability in item fit evaluation

--- k.aefa3.R
+++ k.aefa3.R
@@ -1957,7 +1957,7 @@ surveyFA <- function(data = ..., covdata = NULL, formula = NULL, SE = T,
                      survey.weights = NULL, allowMixedResponse = T, autofix = F,
                      forceUIRT = F, skipIdealPoint = F, MHRM_SE_draws = 1e+4,
                      bifactorSolution = T, skipS_X2 = F, forceNRM = F, needGlobalOptimal = T,
-                     pilotTestMode = F, ...) {
+                     pilotTestMode = F, forceConsiderPositiveZh = F, ...) {
   message('---------------------------------------------------------')
   message(' k.aefa: kwangwoon automated exploratory factor analysis ')
   message('---------------------------------------------------------\n')
@@ -2041,6 +2041,10 @@ surveyFA <- function(data = ..., covdata = NULL, formula = NULL, SE = T,
       
       # item evaluation
       
+      if(S_X2ErrorFlag){
+        message('S_X2 can not be calcuate normally...')
+      }
+      
       # Standardized Log-Likelihood
       if(length(which(surveyFixMod_itemFit$Zh[1:surveyFixMod@Data$nitems] < -1.96)) != 0 && sum(is.na(surveyFixModRAW)) == 0){ # Drasgow, F., Levine, M. V., & Williams, E. A. (1985). Appropriateness measurement with polychotomous item response models and standardized indices. British Journal of Mathematical and Statistical Psychology, 38(1), 67-86.
         message('\nDrasgow, F., Levine, M. V., & Williams, E. A. (1985)')
@@ -2049,6 +2053,8 @@ surveyFA <- function(data = ..., covdata = NULL, formula = NULL, SE = T,
         if(needGlobalOptimal == T && forceUIRT == F){
           surveyFixMod <- deepFA(surveyFixMod, survey.weights)
         }
+        rm(surveyFixMod_itemFit)
+        rm(S_X2ErrorFlag)
       } else if(length(which(surveyFixMod_itemFit$Zh[1:surveyFixMod@Data$nitems] == -Inf)) != 0){ # Drasgow, F., Levine, M. V., & Williams, E. A. (1985). Appropriateness measurement with polychotomous item response models and standardized indices. British Journal of Mathematical and Statistical Psychology, 38(1), 67-86.
         message('\nDrasgow, F., Levine, M. V., & Williams, E. A. (1985)')
         workKeys <- workKeys[-which(min(surveyFixMod_itemFit$Zh[1:surveyFixMod@Data$nitems]) == surveyFixMod_itemFit$Zh[1:surveyFixMod@Data$nitems])]
@@ -2056,7 +2062,28 @@ surveyFA <- function(data = ..., covdata = NULL, formula = NULL, SE = T,
         if(needGlobalOptimal == T && forceUIRT == F){
           surveyFixMod <- deepFA(surveyFixMod, survey.weights)
         }
-      } else if(!S_X2ErrorFlag) { # Chi-squared
+        rm(surveyFixMod_itemFit)
+        rm(S_X2ErrorFlag)
+        
+        
+      } else if(S_X2ErrorFlag) { # if Chi-squared can not be calculate with extremely big Zh
+        if(length(which(surveyFixMod_itemFit$Zh[1:surveyFixMod@Data$nitems] > 1.96)) != 0 && sum(is.na(surveyFixModRAW)) == 0 && forceConsiderPositiveZh){ # Drasgow, F., Levine, M. V., & Williams, E. A. (1985). Appropriateness measurement with polychotomous item response models and standardized indices. British Journal of Mathematical and Statistical Psychology, 38(1), 67-86.
+          message('\nDrasgow, F., Levine, M. V., & Williams, E. A. (1985)')
+          workKeys <- workKeys[-which(max(surveyFixMod_itemFit$Zh[1:surveyFixMod@Data$nitems]) == surveyFixMod_itemFit$Zh[1:surveyFixMod@Data$nitems])]
+          surveyFixMod <- fastFIFA(surveyFixModRAW[,-which(max(surveyFixMod_itemFit$Zh[1:surveyFixMod@Data$nitems]) == surveyFixMod_itemFit$Zh[1:surveyFixMod@Data$nitems])], itemkeys = workKeys, covdata = surveyFixModCOV, formula = formula, SE = SE, SE.type = SE.type, skipNominal = skipNominal, forceGRSM = forceGRSM, assumingFake = assumingFake, masterThesis = masterThesis, forceRasch = forceRasch, unstable = unstable, forceMHRM = forceMHRM, survey.weights = survey.weights, allowMixedResponse = allowMixedResponse, autofix = autofix, forceUIRT = forceUIRT, skipIdealPoint = skipIdealPoint, forceNRM = forceNRM, forceNormalEM = forceNormalEM, ...)
+          if(needGlobalOptimal == T && forceUIRT == F){
+            surveyFixMod <- deepFA(surveyFixMod, survey.weights)
+          }
+          rm(surveyFixMod_itemFit)
+          rm(S_X2ErrorFlag)
+          
+        } else {
+          itemFitDone <- TRUE
+          
+        }
+      } else if(S_X2ErrorFlag == F) { # if Chi-squared can be calculate
+        
+        
         if(sum(is.na(surveyFixMod_itemFit$df.S_X2[1:surveyFixMod@Data$nitems])) != 0 && sum(is.na(surveyFixMod_itemFit$df.S_X2[1:surveyFixMod@Data$nitems])) != surveyFixMod@Data$nitems && surveyFixMod@Model$nfact == 1){
           message('\nremoving items df is NA')
           workKeys <- workKeys[-which(is.na(surveyFixMod_itemFit$df.S_X2) == TRUE)]
@@ -2065,6 +2092,10 @@ surveyFA <- function(data = ..., covdata = NULL, formula = NULL, SE = T,
           if(needGlobalOptimal == T && forceUIRT == F){
             surveyFixMod <- deepFA(surveyFixMod, survey.weights)
           }
+          rm(surveyFixMod_itemFit)
+          rm(S_X2ErrorFlag)
+          
+          
         } else if(sum(na.omit(surveyFixMod_itemFit$df.S_X2[1:surveyFixMod@Data$nitems]) == 0) != 0 && surveyFixMod@Model$nfact == 1 && skipS_X2 == F){
           message('\nremoving items df is 0')
           workKeys <- workKeys[-which(surveyFixMod_itemFit$df.S_X2 == 0)]
@@ -2073,6 +2104,10 @@ surveyFA <- function(data = ..., covdata = NULL, formula = NULL, SE = T,
           if(needGlobalOptimal == T && forceUIRT == F){
             surveyFixMod <- deepFA(surveyFixMod, survey.weights)
           }
+          rm(surveyFixMod_itemFit)
+          rm(S_X2ErrorFlag)
+          
+          
         } else if(sum(is.na(surveyFixMod_itemFit$p.S_X2[1:surveyFixMod@Data$nitems])) != 0 && sum(is.na(surveyFixMod_itemFit$p.S_X2[1:surveyFixMod@Data$nitems])) != surveyFixMod@Data$nitems && surveyFixMod@Model$nfact == 1){
           message('\nremoving items p.S_X2 is NA')
           workKeys <- workKeys[-which(is.na(surveyFixMod_itemFit$p.S_X2) == TRUE)]
@@ -2081,6 +2116,10 @@ surveyFA <- function(data = ..., covdata = NULL, formula = NULL, SE = T,
           if(needGlobalOptimal == T && forceUIRT == F){
             surveyFixMod <- deepFA(surveyFixMod, survey.weights)
           }
+          rm(surveyFixMod_itemFit)
+          rm(S_X2ErrorFlag)
+          
+          
         } else if(sum(is.na(surveyFixMod_itemFit$p.S_X2[1:surveyFixMod@Data$nitems])) == 0 && length(which(surveyFixMod_itemFit$S_X2[1:surveyFixMod@Data$nitems]/surveyFixMod_itemFit$df.S_X2[1:surveyFixMod@Data$nitems] >= 3)) != 0 && skipS_X2 == F && surveyFixMod@Model$nfact == 1){ # Drasgow, F., Levine, M. V., Tsien, S., Williams, B., & Mead, A. D. (1995). Fitting polytomous item response theory models to multiple-choice tests. Applied Psychological Measurement, 19(2), 143-166.
           message('\nDrasgow, F., Levine, M. V., Tsien, S., Williams, B., & Mead, A. D. (1995)')
           workKeys <- workKeys[-which(max(surveyFixMod_itemFit$S_X2[1:surveyFixMod@Data$nitems]/surveyFixMod_itemFit$df.S_X2[1:surveyFixMod@Data$nitems]) == surveyFixMod_itemFit$S_X2[1:surveyFixMod@Data$nitems]/surveyFixMod_itemFit$df.S_X2[1:surveyFixMod@Data$nitems])]
@@ -2089,6 +2128,10 @@ surveyFA <- function(data = ..., covdata = NULL, formula = NULL, SE = T,
           if(needGlobalOptimal == T && forceUIRT == F){
             surveyFixMod <- deepFA(surveyFixMod, survey.weights)
           }
+          rm(surveyFixMod_itemFit)
+          rm(S_X2ErrorFlag)
+          
+          
         } else if(sum(is.na(surveyFixMod_itemFit$p.S_X2[1:surveyFixMod@Data$nitems])) == 0 && length(which(surveyFixMod_itemFit$p.S_X2[1:surveyFixMod@Data$nitems] < .05)) != 0 && skipS_X2 == F && surveyFixMod@Model$nfact == 1){ # Kang, T., & Chen, T. T. (2008). Performance of the Generalized S‐X2 Item Fit Index for Polytomous IRT Models. Journal of Educational Measurement, 45(4), 391-406.; Reise, S. P. (1990). A comparison of item- and person-fit methods of assessing model-data fit in IRT. Applied Psychological Measurement, 14, 127-137.
           message('\nKang, T., & Chen, T. T. (2008); Reise, S. P. (1990)')
           workKeys <- workKeys[-which(max(surveyFixMod_itemFit$S_X2[1:surveyFixMod@Data$nitems]/surveyFixMod_itemFit$df.S_X2[1:surveyFixMod@Data$nitems]) == surveyFixMod_itemFit$S_X2[1:surveyFixMod@Data$nitems]/surveyFixMod_itemFit$df.S_X2[1:surveyFixMod@Data$nitems])]
@@ -2097,7 +2140,16 @@ surveyFA <- function(data = ..., covdata = NULL, formula = NULL, SE = T,
           if(needGlobalOptimal == T && forceUIRT == F){
             surveyFixMod <- deepFA(surveyFixMod, survey.weights)
           }
+          rm(surveyFixMod_itemFit)
+          rm(S_X2ErrorFlag)
+          
+          
+        } else {
+          itemFitDone <- TRUE
+          
         }
+        
+        
       } else if (forceRasch == T) {
         if(length(which(abs(surveyFixMod_itemFit$z.infit[1:surveyFixMod@Data$nitems]) > 2)) != 0){
           
