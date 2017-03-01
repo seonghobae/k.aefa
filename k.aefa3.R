22096d5e17e75bd222ba6475f6f86f60c1c92f24
Seongho <seongho@kw.ac.kr>
2017-03-01 20:31:46 +0900

    Update k.aefa3.R

--- k.aefa3.R
+++ k.aefa3.R
@@ -1927,7 +1927,7 @@ fastFIFA <- function(x, covdata = NULL, formula = NULL, SE = T, SE.type = "Oakes
       return(modTEMP)
     }
     
-    if(i == 1 && modTEMP@OptimInfo$converged == F){
+    if(i == 1 && modTEMP@OptimInfo$converged == F && sum(modTEMP@Model$itemtype %in% 'spline') == 0){
       stop('No convergence')
     }
     
