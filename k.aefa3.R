5044d81338f2fd3665b6ec2a39cd618b873b849a
Seongho <seongho@kw.ac.kr>
2017-04-12 08:11:19 +0900

    disable SE estimation when the forceRasch = T

--- k.aefa3.R
+++ k.aefa3.R
@@ -1432,7 +1432,7 @@ fastFIFA <- function(x, covdata = NULL, formula = NULL, SE = T, SE.type = "Oakes
                                   accelerate = accelerateINPUT, calcNull = T,
                                   technical = list(BURNIN = 1500, SEMCYCLES = 1000, MAXQUAD = 2000000, delta = 1e-20, MHRM_SE_draws = MHRM_SE_draws, symmetric = symmetricINPUT, SEtol = SEtolINPUT,
                                                    removeEmptyRows = removeEmptyRowsConf, NCYCLES = NCYCLES), TOL = TOLINPUT, covdata = covdataINPUT,
-                                  formula = formulaINPUT, optimizer = optimINPUT, solnp_args = optimCTRL, SE = SE,
+                                  formula = formulaINPUT, optimizer = optimINPUT, solnp_args = optimCTRL, SE = F,
                                   SE.type = SE.type, survey.weights = survey.weights, empiricalhist = empiricalhist, ...), silent = F)
         try(return(modTEMP))
       }
